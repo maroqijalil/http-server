1df0c2e6a2ca73401adc9e1e6643143fecbd4432
M Maroqi Abdul Jalil <jalilpro31@gmail.com>
2020-04-12 12:01:43 +0700

    first commit

---
+++ web_server/build/http_server/CMakeFiles/httpd.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/maroqi/Documents/ICHIRO2020/httpd/web_server/http_server/src/simplest_web_server.cpp" "/home/maroqi/Documents/ICHIRO2020/httpd/web_server/build/http_server/CMakeFiles/httpd.dir/src/simplest_web_server.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "SOURCE=\"/home/maroqi/Documents/ICHIRO2020/httpd/web_server\""
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../library/mongoose/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/maroqi/Documents/ICHIRO2020/httpd/web_server/build/library/CMakeFiles/mongoose.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ web_server/build/http_server/CMakeFiles/httpd.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/httpd.dir/src/simplest_web_server.cpp.o"
+  "httpd.pdb"
+  "httpd"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/httpd.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
